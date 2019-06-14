757d43d165a67e1e3bcd489ebaf3494f1b5ab13a
Rshah <passionategirl2000@gmail.com>
2019-06-15 00:17:27 +0530

    web app

---
+++ web1.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask import request
+
+import random
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+
+def randomNumber(rnum):
+    sumran=0
+    for i in range(int(rnum)):
+        sumran += random.randint(0,int(rnum))
+    return sumran
+
+
+@app.route("/r")
+def printRandom():
+    rnum = request.args.get('rnum')
+    print(rnum)
+    var1 =randomNumber(rnum)
+    return str(var1)
+
+if __name__ == '__name__':
+    app.run()
+
+app.run(port=5000)
+
+
